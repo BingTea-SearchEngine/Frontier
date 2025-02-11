48cc2a36b748c7d5f471522b69359babbc5f2a49
Wonbin Jin <wbjin@umich.edu>
2025-02-10 23:53:39 -0500

    init

---
+++ src/Frontier.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "PriorityQueue.hpp"
+
+using std::cout, std::endl;
+
+int main() {
+    cout << "In main" << endl;
+}
---
+++ tests/test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "PriorityQueue.hpp"
+
+TEST(PriorityQueue, Example) {
+    ASSERT_EQ(true, true);
+}
